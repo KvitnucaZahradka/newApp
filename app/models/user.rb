dde353436565a1f752dc7acdf3a758e87a5efee8
KvitnucaZahradka <martin.polacek@stonybrook.edu>
2016-10-31 12:30:24 -0400

    Updade Gemfile for Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+#class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token
+  #before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
-  
+
+
   # Returns the hash digest of the given string
   def User.digest(string)
    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -16,7 +19,7 @@ class User < ActiveRecord::Base
 
   # Returns a random token.
   def User.new_token
-   SecureRandom.urlsafe_base64 
+   SecureRandom.urlsafe_base64
   end
 
   # Remembers a user in the database for the use in the persistent sessions.
@@ -28,11 +31,24 @@ class User < ActiveRecord::Base
   # Returns true if a given token matches the digest
   def authenticated?(remember_token)
    return false if remember_digest.nil?
-   BCrypt::Password.new(remember_digest).is_password?(remember_token) 
+   BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
  # Forgets a user.
  def forget
   update_attribute(:remember_digest, nil)
  end
+
+ # Private methds.
+ private
+  # Converts email to lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
